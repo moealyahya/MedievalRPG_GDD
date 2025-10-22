76acc4d100c3e8a0c1ad753b5d8fbfe88bb3aeb0
hoxtl <neweng1and@icloud.com>
2025-10-22 01:41:43 +0100

    Combat update

---
+++ enemy.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+var speed = 100
+var player_entred = false	
+var player = null	
+var isOnCooldown = false
+var player_in_attack_range = false
+
+func _on_routing_ai_body_entered(body: Node2D) -> void:
+	player_entred = true
+	player = body
+
+
+func _on_routing_ai_body_exited(body: Node2D) -> void:
+	player_entred = false
+	player = null
+
+func _physics_process(delta: float) -> void:
+	if player_entred:
+		position += (player.position - position) / speed
+	if player_in_attack_range:
+		_enemy_attack()
+
+func _enemy_attack () -> bool:
+	if player.has_method("_player_being_hit") && isOnCooldown == false:
+		player._player_being_hit(10, self)
+		isOnCooldown = true
+		$EnemyAttackCooldown.start()
+		return true
+	return false
+
+
+func _on_enemy_attack_cooldown_timeout() -> void:
+	isOnCooldown = false
+
+
+func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("_player_being_hit"):
+		player_in_attack_range = true
+
+
+func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("_player_being_hit"):
+		player_in_attack_range = false
--- player.gd
+++ player.gd
@@ -1,9 +1,8 @@
-class_name Player
-extends Area2D
+extends CharacterBody2D
 
 @export var speed = 400
-@export var atk_growth_per_level = 1.0
-@export var hp_growth_per_level = 1.0
+@export var atk_growth_per_level = 100.0
+@export var hp_growth_per_level = 100.0
 @export var speed_run = 800
 
 var screen_size
@@ -11,8 +10,9 @@ var level = 1
 var atk = level * atk_growth_per_level
 var hp = level * hp_growth_per_level
 var is_running = false
-	
-func playerAttack () -> void:
+var latest_orientation = Vector2.ZERO
+
+func _player_attack () -> void:
 	pass #So far no idea how to implement combat system, gonna try later
 		
 # Called when the node enters the scene tree for the first time.
@@ -21,6 +21,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	_get_latest_orientation()
 	is_running = false
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("run"):
@@ -33,6 +34,9 @@ func _process(delta: float) -> void:
 		velocity.x += 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
+	if Input.is_action_pressed("attack_melee"):
+		_slash_attack()
+		
 		
 	if is_running:
 		velocity = velocity.normalized() * speed_run
@@ -45,3 +49,44 @@ func _process(delta: float) -> void:
 	
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+
+
+func _on_player_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		print("enterd")
+	pass # Replace with function body.
+
+
+func _on_player_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		print("left")
+	pass # Replace with function body.
+
+func _player_being_hit (damage : int, attacker : Node2D) -> Node2D:
+	hp -= damage
+	print("Being hit! HP - " + str(damage) + ", has " + str(hp) + " left.")
+	return attacker
+
+func _get_latest_orientation () -> void:
+	var input_dir = Vector2.ZERO
+	input_dir.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_dir.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	input_dir = input_dir.normalized()
+	
+	if input_dir != Vector2.ZERO:
+		latest_orientation = input_dir
+	else:
+		latest_orientation = Vector2.ZERO
+	
+func _slash_attack() -> void:
+	$AttackHitbox/CollisionShape2D.global_position = global_position + latest_orientation * 140
+	$AttackHitbox/CollisionShape2D.rotation = latest_orientation.angle()
+	if !Input.is_action_pressed("attack_melee"):
+		$AttackHitbox/CollisionShape2D.global_position = Vector2.ZERO
+		$AttackHitbox/CollisionShape2D.rotation = Vector2.ZERO.angle()
+	# Optionally resize hitbox if it has a CollisionShape2D child
+	#var shape = hitbox.get_node_or_null("CollisionShape2D")
+	#if shape and shape.shape is RectangleShape2D:
+		#shape.shape.extents = hitbox_size / 2
+
+	# Schedule deletion after duration
